abd4d202f6b99ea509be47f9638fd24b14c289c4
Eraldo Hasanaj <eraldo_hasanaj@hotmail.com>
2025-07-19 22:27:33 +0200

    Added support for go lang

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "go",
+  callback = function()
+    vim.keymap.set("n", "<leader>ci", "<cmd>GoInstall<cr>", { desc = "Install Go dependencies" })
+    vim.keymap.set("n", "<leader>cr", "<cmd>GoRun<cr>", { desc = "Run Go file" })
+    vim.keymap.set("n", "<leader>ct", "<cmd>GoTest<cr>", { desc = "Test Go file" })
+  end,
+})
