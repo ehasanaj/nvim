ecfcaa08c1ce7fb088aa62acb70bad2f7f2fcf1b
Eraldo Hasanaj <eraldo_hasanaj@hotmail.com>
2025-07-19 22:39:47 +0200

    Added jj and jk keymaps of escaping insert mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,3 +10,6 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.keymap.set("n", "<leader>ct", "<cmd>GoTest<cr>", { desc = "Test Go file" })
   end,
 })
+
+vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
+vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = false })
