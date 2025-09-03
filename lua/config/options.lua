5a66b994fb764eb6e5d899e3a7c60ffbf4154e3f
Eraldo Hasanaj <eraldo_hasanaj@hotmail.com>
2025-09-03 10:31:08 +0200

    Added folding support

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.winbar = "%=%m %f"
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+vim.o.foldenable = true
+vim.o.foldlevel = 99
