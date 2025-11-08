9c197c768ed6f614a354f5a2f36ce1b5b8ef0613
Eraldo Hasanaj <eraldo_hasanaj@hotmail.com>
2025-11-08 23:02:31 +0100

    Configured snacks

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,24 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    scroll = {
+      enabled = false,
+    },
+    dashboard = {
+      enabled = true,
+      sections = {
+        { section = "header" },
+        { section = "keys", gap = 1, padding = 1 },
+        { section = "startup" },
+        {
+          section = "terminal",
+          cmd = "ascii-image-converter logo.png -C -b --dither",
+          random = 10,
+          pane = 2,
+          indent = 4,
+          height = 30,
+        },
+      },
+    },
+  },
+}
