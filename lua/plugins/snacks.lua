cecf8fb40b8e4d7dc67c8764741a475fdfe28712
Eraldo Hasanaj <eraldo_hasanaj@hotmail.com>
2025-11-17 13:33:50 +0100

    Updated snacks plugin

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -12,7 +12,7 @@ return {
         { section = "startup" },
         {
           section = "terminal",
-          cmd = "ascii-image-converter logo.png -C -b --dither",
+          cmd = "ascii-image-converter ~/.config/nvim/logo.png -C -b --dither",
           random = 10,
           pane = 2,
           indent = 4,
