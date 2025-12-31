0fa39733d868ddda3900cb3c988abed74380b204
Eraldo Hasanaj <eraldo_hasanaj@hotmail.com>
2025-12-31 15:35:19 +0100

    Nord

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,7 +2,9 @@ return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
   -- add nord theme
-  { "arcticicestudio/nord-vim", name = "nord" },
+  --{ "arcticicestudio/nord-vim", name = "nord" },
+  -- add alternarive nord.nvim theme
+  { "shaunsingh/nord.nvim", name = "nord" },
 
   -- Configure LazyVim to load gruvbox
   {
