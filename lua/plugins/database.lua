4851f893ada6700eea1374caf42f82e77b52d55f
Eraldo Hasanaj <eraldo_hasanaj@hotmail.com>
2026-01-29 01:20:12 +0100

    Added database support with dadbot

---
+++ lua/plugins/database.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "tpope/vim-dadbod",
+    lazy = true,
+  },
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+    },
+    cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
+    init = function()
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+    keys = {
+      { "<leader>Du", "<cmd>DBUIToggle<cr>", desc = "Toggle DBUI" },
+      { "<leader>Da", "<cmd>DBUIAddConnection<cr>", desc = "Add DB Connection" },
+      { "<leader>Df", "<cmd>DBUIFindBuffer<cr>", desc = "Find DB Buffer" },
+    },
+  },
+  {
+    "saghen/blink.cmp",
+    optional = true,
+    opts = {
+      sources = {
+        default = { "dadbod" },
+        providers = {
+          dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+        },
+      },
+    },
+  },
+}
