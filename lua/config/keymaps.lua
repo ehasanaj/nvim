0dc5c79d36c13821882890eccd28d0c21ad6371f
Eraldo Hasanaj <eraldo_hasanaj@hotmail.com>
2026-03-20 11:26:10 +0100

    Added support for google drive sync shell execution through rclone

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,33 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local function run_dir_sync(command, title)
+  local cwd = vim.fn.getcwd()
+
+  Snacks.notify.info(string.format("Running `%s` in %s", command, cwd), {
+    title = title,
+  })
+
+  Snacks.terminal({ vim.o.shell, "-ic", command }, {
+    cwd = cwd,
+    auto_close = false,
+    win = {
+      title = title,
+    },
+  })
+end
+
+vim.api.nvim_create_user_command("GPushDir", function()
+  run_dir_sync("gpushdir", "GDrive Push")
+end, { desc = "Push current directory to Google Drive" })
+
+vim.api.nvim_create_user_command("GPullDir", function()
+  run_dir_sync("gpulldir", "GDrive Pull")
+end, { desc = "Pull current directory from Google Drive" })
+
+vim.keymap.set("n", "<leader>mu", "<cmd>GPushDir<cr>", { desc = "Google Drive Push Dir", silent = true })
+vim.keymap.set("n", "<leader>md", "<cmd>GPullDir<cr>", { desc = "Google Drive Pull Dir", silent = true })
+
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "go",
   callback = function()
