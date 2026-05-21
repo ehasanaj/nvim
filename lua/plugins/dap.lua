3e8267dcb4491478ae808e7bc59496b76f09ab1f
Eraldo Hasanaj <eraldo_hasanaj@hotmail.com>
2026-05-21 07:45:55 +0200

    Added go debugger

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "leoluz/nvim-dap-go",
+    ft = "go",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+    },
+    config = function()
+      require("dap-go").setup()
+    end,
+  },
+}
