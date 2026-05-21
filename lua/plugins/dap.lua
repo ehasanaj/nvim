852787b1301c6ef62887e283be81be6fc69f1e0c
Eraldo Hasanaj <eraldo_hasanaj@hotmail.com>
2026-05-21 08:21:39 +0200

    Added go debugger

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,12 +1,98 @@
 return {
   {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "rcarriga/nvim-dap-ui",
+      "theHamsta/nvim-dap-virtual-text",
+    },
+    -- stylua: ignore
+    keys = {
+      { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+      { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, desc = "Conditional Breakpoint" },
+      { "<leader>dc", function() require("dap").continue() end, desc = "Debug Continue/Start" },
+      { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
+      { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
+      { "<leader>do", function() require("dap").step_over() end, desc = "Step Over" },
+      { "<leader>dO", function() require("dap").step_out() end, desc = "Step Out" },
+      { "<leader>dp", function() require("dap").pause() end, desc = "Pause Debugger" },
+      { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle DAP REPL" },
+      { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last Debug" },
+      { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate Debugger" },
+      { "<leader>du", function() require("dapui").toggle() end, desc = "Toggle DAP UI" },
+      { "<leader>de", function() require("dapui").eval() end, desc = "DAP Eval", mode = { "n", "v" } },
+      { "<leader>dh", function() require("dap.ui.widgets").hover() end, desc = "DAP Hover" },
+    },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+
+      require("nvim-dap-virtual-text").setup()
+      dapui.setup()
+
+      vim.fn.sign_define("DapBreakpoint", { text = "B", texthl = "DiagnosticError" })
+      vim.fn.sign_define("DapBreakpointCondition", { text = "C", texthl = "DiagnosticWarn" })
+      vim.fn.sign_define("DapLogPoint", { text = "L", texthl = "DiagnosticInfo" })
+      vim.fn.sign_define("DapStopped", { text = ">", texthl = "DiagnosticInfo", linehl = "Visual" })
+      vim.fn.sign_define("DapBreakpointRejected", { text = "R", texthl = "DiagnosticError" })
+
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+
+      local vscode = require("dap.ext.vscode")
+      local json = require("plenary.json")
+      vscode.json_decode = function(str)
+        return vim.json.decode(json.json_strip_comments(str))
+      end
+    end,
+  },
+  {
     "leoluz/nvim-dap-go",
     ft = "go",
     dependencies = {
       "mfussenegger/nvim-dap",
     },
-    config = function()
-      require("dap-go").setup()
+    opts = {
+      dap_configurations = {
+        {
+          type = "go",
+          name = "Attach remote",
+          mode = "remote",
+          request = "attach",
+        },
+      },
+      tests = {
+        verbose = true,
+      },
+    },
+    -- stylua: ignore
+    keys = {
+      { "<leader>dgt", function() require("dap-go").debug_test() end, desc = "Debug Go Test" },
+      { "<leader>dgl", function() require("dap-go").debug_last_test() end, desc = "Debug Last Go Test" },
+    },
+    config = function(_, opts)
+      require("dap-go").setup(opts)
+    end,
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "delve" })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "go", "gomod", "gowork", "gosum" })
     end,
   },
 }
